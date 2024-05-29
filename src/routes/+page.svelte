6469dfdfade587cfd9303f8cc94875d3546b9d6e
NazirArifin <ceylon.rizan@gmail.com>
2024-05-30 06:28:40 +0700

    refactor item todo into list item instead of div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   
-  
 
 </script>
 
@@ -22,22 +21,26 @@
   </div>
 
   <!-- item todo  -->
-  <div class="w-3/4 mx-auto pt-5">
-    <label class="flex items-center">
-      <input type="checkbox" class="hidden peer" checked>
-      <span class="w-10 h-10 border-2 cursor-pointer bg-white border-gray-600 *:peer-checked:text-gray-600">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white">
-          <path d="M20 6 9 17l-5-5"/>
-        </svg>
-      </span>
-      <span class="ml-2 text-base font-semibold text-gray-600">Buat starter app dengan Svelte</span>
-    </label>
+  <ul>
+    
+    <li class="w-3/4 mx-auto pt-5">
+      <label class="flex items-center">
+        <input type="checkbox" class="hidden peer" checked>
+        <span class="w-10 h-10 border-2 cursor-pointer bg-white border-gray-600 *:peer-checked:text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white">
+            <path d="M20 6 9 17l-5-5"/>
+          </svg>
+        </span>
+        <span class="ml-2 text-base font-semibold text-gray-600">Buat starter app dengan Svelte</span>
+      </label>
 
-    <div class="flex gap-2 mt-3">
-      <button class="border-black border grow bg-white p-2 hover:bg-gray-100">Edit</button>
-      <button class="border-black border grow bg-red-900 text-white p-2 hover:bg-red-800">Hapus</button>
-    </div>
-  </div>
+      <div class="flex gap-2 mt-3">
+        <button class="border-black border grow bg-white p-2 hover:bg-gray-100">Edit</button>
+        <button class="border-black border grow bg-red-900 text-white p-2 hover:bg-red-800">Hapus</button>
+      </div>
+    </li>
+    
+  </ul>
 
   <!-- tombol aksi  -->
   <div class="w-3/4 mx-auto">
