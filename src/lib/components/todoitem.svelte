5180f7fbeb74035fc7a3bceb636f26bb2c2f8132
NazirArifin <ceylon.rizan@gmail.com>
2024-06-06 05:50:43 +0700

    dispatch examples

---
+++ src/lib/components/todoitem.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Todo } from "$lib/types/todo";
+  import { createEventDispatcher } from "svelte";
+  
+  export let todo: Todo;
+  const dispatch = createEventDispatcher();
+
+</script>
+
+<li class="w-3/4 mx-auto pt-5">
+  <button class="flex items-center" on:click={() => todo.completed = !todo.completed}>
+    <input type="checkbox" class="hidden peer" checked={todo.completed}>
+    <span class="w-10 h-10 border-2 cursor-pointer bg-white border-gray-600 *:peer-checked:text-gray-600">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white">
+        <path d="M20 6 9 17l-5-5"/>
+      </svg>
+    </span>
+    <span class="ml-2 text-base font-semibold text-gray-600">{todo.text}</span>
+  </button>
+
+  <div class="flex gap-2 mt-3">
+    <button on:click={() => dispatch('edit', todo.id)} class="border-black border grow bg-white p-2 hover:bg-gray-100">Edit</button>
+    <button on:click={() => dispatch('delete', todo.id)} class="border-black border grow bg-red-900 text-white p-2 hover:bg-red-800">Hapus</button>
+  </div>
+</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
+  import Actionbuttons from "$lib/components/actionbuttons.svelte";
   import Filterbuttons from "$lib/components/filterbuttons.svelte";
-
-  type Todo = {
-    id: number;
-    text: string;
-    completed: boolean;
-  }
-
+  import Todoitem from "$lib/components/todoitem.svelte";
+  import type { Todo } from "$lib/types/todo";
 
   let todos: Todo[] = [
     { id: 1, text: 'Buat starter app dengan Svelte', completed: true },
@@ -27,7 +23,7 @@
     todos = [
       ...todos,
       {
-        id: todos.length + 1,
+        id: Date.now() / 1000,
         text,
         completed: false
       }
@@ -64,6 +60,14 @@
     return todos;
   }
 
+  function checkAll() {
+    todos = todos.map(todo => ({ ...todo, completed: true }));
+  }
+
+  function deleteAll() {
+    todos = todos.filter(todo => !todo.completed);
+  }
+
 </script>
 
 <main class="container p-7 bg-gray-100 shadow-lg mx-auto my-5">
@@ -74,7 +78,13 @@
   {#if !sedangMengedit}
   <button class="w-full bg-slate-800 text-gray-400 mt-2 py-3 hover:bg-slate-950" on:click={() => tambahTodo()}>Tambah</button>
   {:else}
-  <button class="w-full bg-blue-800 text-gray-400 mt-2 py-3 hover:bg-slate-950" on:click={() => simpanTodo()}>Simpan</button>
+  <div class="flex gap-2">
+    <button class="w-full bg-blue-800 text-gray-400 mt-2 py-3 hover:bg-slate-950" on:click={() => simpanTodo()}>Simpan</button>
+    <button class="w-full bg-red-800 text-gray-400 mt-2 py-3 hover:bg-slate-950" on:click={() => {
+      sedangMengedit = false;
+      text = '';
+    }}>Batal</button>
+  </div>
   {/if}
 
   <!-- tombol filter -->
@@ -87,22 +97,8 @@
   <!-- item todo  -->
   <ul>
     {#each filterTodos(filter, todos) as todo, index(todo.id)}
-    <li class="w-3/4 mx-auto pt-5">
-      <button class="flex items-center" on:click={() => todo.completed = !todo.completed}>
-        <input type="checkbox" class="hidden peer" checked={todo.completed}>
-        <span class="w-10 h-10 border-2 cursor-pointer bg-white border-gray-600 *:peer-checked:text-gray-600">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white">
-            <path d="M20 6 9 17l-5-5"/>
-          </svg>
-        </span>
-        <span class="ml-2 text-base font-semibold text-gray-600">{todo.text}</span>
-      </button>
-
-      <div class="flex gap-2 mt-3">
-        <button on:click={() => editTodo(todo.id)} class="border-black border grow bg-white p-2 hover:bg-gray-100">Edit</button>
-        <button on:click={() => hapusTodo(todo.id)} class="border-black border grow bg-red-900 text-white p-2 hover:bg-red-800">Hapus</button>
-      </div>
-    </li>
+    
+    <Todoitem {todo} on:delete={(e) => hapusTodo(e.detail)} on:edit={(e) => editTodo(e.detail)} />
 
     {:else}
     <p class="text-center text-red-600 font-semibold text-3xl mt-5">Tidak ada data</p>
@@ -111,13 +107,6 @@
   </ul>
 
   <!-- tombol aksi  -->
-  <div class="w-3/4 mx-auto">
-    <hr class="border-black my-7">
-
-    <label class="flex items-center gap-2">
-      <button class="grow bg-black text-white p-2 hover:bg-gray-900">Pilih Semua</button>
-      <button class="grow bg-black text-white p-2 hover:bg-gray-900">Hapus Diselesaikan</button>
-    </label>
-  </div>
+  <Actionbuttons on:checkAll={() => checkAll()} on:deleteCompleted={() => deleteAll()} />
 
 </main>
