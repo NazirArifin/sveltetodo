59f00168638da23c741e38359a3369694daf8f6a
NazirArifin <ceylon.rizan@gmail.com>
2024-06-06 04:44:00 +0700

    edit delete item

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,62 @@
     { id: 2, text: 'Buat component pertama kali', completed: true },
     { id: 3, text: 'Selesaikan semua tutorial', completed: false },
   ];
-  let totalTodos = todos.length;
-  let completedTodos = todos.filter(todo => todo.completed).length;
+  $: totalTodos = todos.length;
+  $: completedTodos = todos.filter(todo => todo.completed).length;
+
+  function hapusTodo(id: number) {
+    todos = todos.filter(todo => todo.id !== id);
+  }
+
+  let text = '';
+  function tambahTodo() {
+    if (text.trim() === '') return;
+
+    todos = [
+      ...todos,
+      {
+        id: todos.length + 1,
+        text,
+        completed: false
+      }
+    ];
+
+    text = '';
+  }
+
+  let sedangMengedit = false;
+  let todoId = 0;
+  function editTodo(id: number) {
+    sedangMengedit = true;
+    todoId = id;
+    text = todos.find(todo => todo.id === id)!.text;
+  }
+  function simpanTodo() {
+    const todo = todos.find(todo => todo.id === todoId);
+    const index = todos.findIndex(todo => todo.id === todoId);
+    if (todo) {
+      todo.text = text;
+    }
+    const tempTodos = [...todos];
+    tempTodos[index] = todo!;
+    todos = tempTodos;
+
+    sedangMengedit = false;
+    text = '';
+  }
 
 </script>
 
 <main class="container p-7 bg-gray-100 shadow-lg mx-auto my-5">
 
   <h1 class="text-center text-2xl text-gray-600">Apa yang harus dikerjakan?</h1>
-  <input type="text" class="border-black border-2 mt-7 w-full py-5">
-  <button class="w-full bg-slate-800 text-gray-400 mt-2 py-3 hover:bg-slate-950">Tambah</button>
+  <input type="text" bind:value={text} class="border-black border-2 mt-7 w-full py-5">
+  
+  {#if !sedangMengedit}
+  <button class="w-full bg-slate-800 text-gray-400 mt-2 py-3 hover:bg-slate-950" on:click={() => tambahTodo()}>Tambah</button>
+  {:else}
+  <button class="w-full bg-blue-800 text-gray-400 mt-2 py-3 hover:bg-slate-950" on:click={() => simpanTodo()}>Simpan</button>
+  {/if}
 
   <!-- tombol filter -->
   <div class="flex mx-auto w-3/4 pt-3 gap-2">
@@ -33,9 +79,9 @@
 
   <!-- item todo  -->
   <ul>
-    {#each todos as todo, index (todo.id)}
+    {#each todos as todo, index(todo.id)}
     <li class="w-3/4 mx-auto pt-5">
-      <label class="flex items-center">
+      <button class="flex items-center" on:click={() => todo.completed = !todo.completed}>
         <input type="checkbox" class="hidden peer" checked={todo.completed}>
         <span class="w-10 h-10 border-2 cursor-pointer bg-white border-gray-600 *:peer-checked:text-gray-600">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white">
@@ -43,15 +89,17 @@
           </svg>
         </span>
         <span class="ml-2 text-base font-semibold text-gray-600">{todo.text}</span>
-      </label>
+      </button>
 
       <div class="flex gap-2 mt-3">
-        <button class="border-black border grow bg-white p-2 hover:bg-gray-100">Edit</button>
-        <button class="border-black border grow bg-red-900 text-white p-2 hover:bg-red-800">Hapus</button>
+        <button on:click={() => editTodo(todo.id)} class="border-black border grow bg-white p-2 hover:bg-gray-100">Edit</button>
+        <button on:click={() => hapusTodo(todo.id)} class="border-black border grow bg-red-900 text-white p-2 hover:bg-red-800">Hapus</button>
       </div>
     </li>
+
     {:else}
     <p class="text-center text-red-600 font-semibold text-3xl mt-5">Tidak ada data</p>
+
     {/each}
   </ul>
 
